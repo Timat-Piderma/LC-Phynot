2368ecc0f0c7251d9bd869cffc7ed15dad96ece5
Timat-Piderma <mttpecc@gmail.com>
2025-02-26 16:19:20 +0100

    Update TypeSystem.hs
    Indentation

--- TypeSystem.hs
+++ TypeSystem.hs
@@ -33,7 +33,7 @@ sup (POINTER t1) (POINTER t2) =
     then POINTER t1
   else Base (ERROR ("Pointer types "++ typeToString t1 ++ " and " ++ typeToString t2 ++ " do not match"))
 
-sup (ADDRESS t1) (POINTER t2)  =  
+sup (ADDRESS t1) (POINTER t2) =  
   if t1 == t2
     then POINTER t1
   else Base (ERROR ("Pointer of Type "++ typeToString t2 ++ " is pointing to an Address of Type " ++ typeToString t1))
@@ -49,71 +49,71 @@ sup _ (Base (ERROR s))            = Base (ERROR s)
 sup t1 t2                         = Base (ERROR ("Type mismatch: " ++ typeToString t1 ++ " and " ++ typeToString t2 ++ " are not compatible"))
 
 getArrayCurrentType :: Type -> Int -> Type
-getArrayCurrentType t 0 = t
-getArrayCurrentType (ARRAY t) n = getArrayCurrentType t (n - 1)
-getArrayCurrentType _ _ = Base (ERROR "Invalid array access")
+getArrayCurrentType t 0           = t
+getArrayCurrentType (ARRAY t) n   = getArrayCurrentType t (n - 1)
+getArrayCurrentType _ _           = Base (ERROR "Invalid array access")
 
 -- Given a type, returns a string representation of it
 typeToString :: Type -> String
-typeToString (Base (ERROR s))  = s
-typeToString (Base INT)    = "Integer"
-typeToString (Base FLOAT)  = "Double"
-typeToString (Base BOOL)   = "Boolean"
-typeToString (Base CHAR)   = "Character"
-typeToString (Base STRING) = "String"
-typeToString (Base NONE)   = "None"
-typeToString (ARRAY t2) = "Array of " ++ typeToString t2;
-typeToString (POINTER t) = "Pointer to " ++ typeToString t;
-typeToString (ADDRESS t) = "Address of " ++ typeToString t;
+typeToString (Base (ERROR s))     = s
+typeToString (Base INT)           = "Integer"
+typeToString (Base FLOAT)         = "Double"
+typeToString (Base BOOL)          = "Boolean"
+typeToString (Base CHAR)          = "Character"
+typeToString (Base STRING)        = "String"
+typeToString (Base NONE)          = "None"
+typeToString (ARRAY t2)           = "Array of " ++ typeToString t2;
+typeToString (POINTER t)          = "Pointer to " ++ typeToString t;
+typeToString (ADDRESS t)          = "Address of " ++ typeToString t;
 
 -- Given a type, returns the basic arithmetic type
-mathtype :: BasicType -> BasicType
-mathtype FLOAT      = FLOAT
-mathtype INT        = INT
-mathtype BOOL       = INT
-mathtype CHAR       = INT
-mathtype _          = ERROR "Cannot perform arithmetic operations on this type"
+mathtype :: Type -> Type
+mathtype (Base FLOAT)             = Base FLOAT
+mathtype (Base INT)               = Base INT
+mathtype (Base BOOL)              = Base INT
+mathtype (Base CHAR)              = Base INT
+mathtype t                        = Base (ERROR ("Cannot perform arithmetic operations on type '" ++ typeToString t ++ "'"))
 
 -- Given two types, returns the BOOL if they are compatible
 rel :: Type -> Type -> Type
-rel (Base BOOL) (Base BOOL) = Base (ERROR "Cannot compare with boolean values")
-rel (Base BOOL) _ = Base (ERROR "Cannot compare with boolean values")
-rel _ (Base BOOL) = Base (ERROR "Cannot compare with boolean values")
+rel (Base BOOL) (Base BOOL)       = Base (ERROR "Cannot compare with boolean values")
+rel (Base BOOL) _                 = Base (ERROR "Cannot compare with boolean values")
+rel _ (Base BOOL)                 = Base (ERROR "Cannot compare with boolean values")
 rel x y = case sup x y of
-  Base (ERROR d)            -> Base (ERROR d)
-  _                         -> Base BOOL
+  Base (ERROR d)                  -> Base (ERROR d)
+  _                               -> Base BOOL
 
 -- Checks if a value is BOOL
 isBoolean :: Type -> Bool
-isBoolean (Base BOOL) = True
-isBoolean _ = False
+isBoolean (Base BOOL)             = True
+isBoolean _                       = False
 
 -- Checks if a value is INT
 isInt :: Type -> Bool
-isInt (Base INT) = True
-isInt _ = False
+isInt (Base INT)                  = True
+isInt _                           = False
 
 -- Checks if a value is FLOAT
 isFloat :: Type -> Bool
-isFloat (Base FLOAT) = True
-isFloat _ = False
+isFloat (Base FLOAT)              = True
+isFloat _                         = False
 
 -- Checks if a value is CHAR
 isChar :: Type -> Bool
-isChar (Base CHAR) = True
-isChar _ = False
+isChar (Base CHAR)                = True
+isChar _                          = False
 
 -- Checks if a value is STRING
 isString :: Type -> Bool
-isString (Base STRING) = True
-isString _ = False
+isString (Base STRING)            = True
+isString _                        = False
 
 -- Checks if a value is an ERROR
 isERROR :: Type -> Bool
-isERROR (Base (ERROR _)) = True
-isERROR _ = False
+isERROR (Base (ERROR _))          = True
+isERROR _                         = False
 
 -- Checks if a value is a ARRAY
 isArray :: Type -> Bool
-isArray (ARRAY _) = True
-isArray _ = False
+isArray (ARRAY _)                 = True
+isArray _                         = False
